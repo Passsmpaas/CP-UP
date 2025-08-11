1fe51b0a8639aba65ccb712b5103b93e52c79ae0
Passsmpaas <lokeshsingh17628@gmail.com>
2025-08-11 09:30:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -469,4 +469,336 @@ async def id_command(client, message: Message):
 @bot.on_message(filters.private & filters.command(["info"]))
 async def info(bot: Client, update: Message):
     
-    text
+    text = (
+        f"╭────────────────╮\n"
+        f"│✨ **Your Telegram Info**✨ \n"
+        f"├────────────────\n"
+        f"├🔹**Name :** `{update.from_user.first_name} {update.from_user.last_name if update.from_user.last_name else 'None'}`\n"
+        f"├🔹**User ID :** @{update.from_user.username}\n"
+        f"├🔹**TG ID :** `{update.from_user.id}`\n"
+        f"├🔹**Profile :** {update.from_user.mention}\n"
+        f"╰────────────────╯"
+    )
+    
+    await update.reply_text(        
+        text=text,
+        disable_web_page_preview=True,
+        reply_markup=BUTTONSCONTACT
+    )
+
+@bot.on_message(filters.command(["help"]))
+async def txt_handler(client: Client, m: Message):
+    await bot.send_message(m.chat.id, text= (
+        f"╭━━━━━━━✦✧✦━━━━━━━╮\n"
+        f" 𝐌𝐫𝐒𝐭𝐫𝐚𝐧𝐠𝐞𝐫™❤️\n"
+        f"╰━━━━━━━✦✧✦━━━━━━━╯\n"
+        f"▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰\n" 
+        f"╭━━━━━━━━━━━━━ ❀° ━━━╮\n"
+        f"┣⪼📌 𝗠𝗮𝗶𝗻 𝗙𝗲𝗮𝘁𝘂𝗿𝗲𝘀:\n\n"  
+        f"┣⪼ /start – Bot Status Check\n"
+        f"┣⪼ /drm – Extract from .txt (Auto)\n"
+        f"┣⪼ /y2t – YouTube → .txt Converter\n"  
+        f"┣⪼ /t2t – Text → .txt Generator\n" 
+        f"┣⪼ /stop – Cancel Running Task\n"
+        f"╰━━━━━━━━━━━━━ ❀° ━━━╯シ\n"
+        f"▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰ \n"
+        f"╭━━━━━━━━━━━━━ ❀° ━━━╮\n"
+        f"┣⪼⚙️ 𝗧𝗼𝗼𝗹𝘀 & 𝗦𝗲𝘁𝘁𝗶𝗻𝗴𝘀: \n\n" 
+        f"┣⪼ /cookies – Update YT Cookies\n" 
+        f"┣⪼ /id – Get Chat/User ID\n"  
+        f"┣⪼ /info – User Details\n"  
+        f"┣⪼ /logs – View Bot Activity\n"
+        f"╰━━━━━━━━━━━━━ ❀° ━━━╯シ\n"
+        f"▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰\n"
+        f"╭━━━━━━━━━━━━━ ❀° ━━━╮\n"
+        f"┣⪼👤 𝐔𝐬𝐞𝐫 𝐀𝐮𝐭𝐡𝐞𝐧𝐭𝐢𝐜𝐚𝐭𝐢𝐨𝐧: **(OWNER)**\n\n" 
+        f"┣⪼ /addauth xxxx -Add User telegram number\n"
+        f"┣⪼/addauth xxxx -📱whatsapp 📞number\n"
+        f"┣⪼🤖 B0T OWNER ━➣ 𝐌𝐫𝐒𝐭𝐫𝐚𝐧𝐠𝐞𝐫™❤️\n"
+        f"┣⪼ /rmauth xxxx – Remove User ID\n"  
+        f"┣⪼ /users – Total User List\n"
+        f"╰━━━━━━━━━━━━━ ❀° ━━━╯シ\n"
+        f"▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰\n"
+        f"╭━━━━━━━━━━━━━ ❀° ━━━╮\n"
+        f"┣⪼💡 𝗡𝗼𝘁𝗲:\n\n"  
+        f"┣⪼• Send any link for auto-extraction\n"  
+        f"┣⪼• Supports batch processing\n\n"
+        f"╰━━━━━━━━━━━━━ ❀° ━━━╯シ\n"
+        f"╭────────⊰◆⊱────────╮\n"   
+        f" ➠ 𝐌𝐚𝐝𝐞 𝐁𝐲 : 𝐌𝐫𝐒𝐭𝐫𝐚𝐧𝐠𝐞𝐫™❤️ 💻\n"
+        f"╰────────⊰◆⊱────────╯\n"
+        )
+    )                    
+          
+@bot.on_message(filters.command(["logs"]))
+async def send_logs(client: Client, m: Message):  # Correct parameter name
+    try:
+        with open("logs.txt", "rb") as file:
+            sent = await m.reply_text("**📤 Sending you ....**")
+            await m.reply_document(document=file)
+            await sent.delete()
+    except Exception as e:
+        await m.reply_text(f"Error sending logs: {e}")
+
+@bot.on_message(filters.command(["drm"]) )
+async def txt_handler(bot: Client, m: Message):  
+    editable = await m.reply_text(f"__Hii, I am drm Downloader Bot__\n\n<i>Send Me Your txt file which enclude Name with url...\nE.g: Name: Link</i>")
+    input: Message = await bot.listen(editable.chat.id)
+    x = await input.download()
+    await input.delete(True)
+    file_name, ext = os.path.splitext(os.path.basename(x))  # Extract filename & extension
+    path = f"./downloads/{m.chat.id}"
+    pdf_count = 0
+    img_count = 0
+    other_count = 0
+    
+    try:    
+        with open(x, "r") as f:
+            content = f.read()
+        content = content.split("\n")
+        
+        links = []
+        for i in content:
+            if "://" in i:
+                url = i.split("://", 1)[1]
+                links.append(i.split("://", 1))
+                if ".pdf" in url:
+                    pdf_count += 1
+                elif url.endswith((".png", ".jpeg", ".jpg")):
+                    img_count += 1
+                else:
+                    other_count += 1
+        os.remove(x)
+    except:
+        await m.reply_text("```🔹Invalid file input.```")
+        os.remove(x)
+        return
+    
+    await editable.edit(f"Total 🔗 links found are {len(links)}\nSend From where you want to download.initial is 1")
+    if m.chat.id not in AUTH_USERS:
+        print(f"User ID not in AUTH_USERS", m.chat.id)
+        await bot.send_message(m.chat.id, f"__Oopss! You are not a Premium member __\n__PLEASE /upgrade YOUR PLAN__\n__Send me your user id for authorization__\n__Your User id__ - `{m.chat.id}`\n")
+        return
+    input0: Message = await bot.listen(editable.chat.id)
+    raw_text = input0.text
+    await input0.delete(True)
+           
+    await editable.edit("__Enter Batch Name or send /d for grabbing from text filename.__")
+    input1: Message = await bot.listen(editable.chat.id)
+    raw_text0 = input1.text
+    await input1.delete(True)
+    if raw_text0 == '/d':
+        b_name = file_name.replace('_', ' ')
+    else:
+        b_name = raw_text0
+
+    await editable.edit("__Enter resolution or Video Quality (`144`, `240`, `360`, `480`, `720`, `1080`)__")
+    input2: Message = await bot.listen(editable.chat.id)
+    raw_text2 = input2.text
+    quality = f"{raw_text2}p"
+    await input2.delete(True)
+    try:
+        if raw_text2 == "144":
+            res = "256x144"
+        elif raw_text2 == "240":
+            res = "426x240"
+        elif raw_text2 == "360":
+            res = "640x360"
+        elif raw_text2 == "480":
+            res = "854x480"
+        elif raw_text2 == "720":
+            res = "1280x720"
+        elif raw_text2 == "1080":
+            res = "1920x1080" 
+        else: 
+            res = "UN"
+    except Exception:
+            res = "UN"
+
+    await editable.edit("__Enter Your Channel Name or Owner Name__\n__Send /d for use default__")
+    input3: Message = await bot.listen(editable.chat.id)
+    raw_text3 = input3.text
+    await input3.delete(True)
+    if raw_text3 == '/d':
+        CR = f"{CREDIT}"
+    else:
+        CR = raw_text3
+
+    await editable.edit("🔹Enter Your PW Token For 𝐌𝐏𝐃 𝐔𝐑𝐋\n🔹Send /anything for use default")
+    input4: Message = await bot.listen(editable.chat.id)
+    raw_text4 = input4.text
+    await input4.delete(True)
+
+    await editable.edit(f"Send the Video Thumb URL\nSend /d for use default\n\nYou can direct upload thumb\nSend **No** for use default")
+    input6 = message = await bot.listen(editable.chat.id)
+    raw_text6 = input6.text
+    await input6.delete(True)
+
+    if input6.photo:
+        thumb = await input6.download()  # Use the photo sent by the user
+    elif raw_text6.startswith("http://") or raw_text6.startswith("https://"):
+        # If a URL is provided, download thumbnail from the URL
+        getstatusoutput(f"wget '{raw_text6}' -O 'thumb.jpg'")
+        thumb = "thumb.jpg"
+    else:
+        thumb = raw_text6
+
+    await editable.edit("__Please Provide Channel id or where you want to Upload video or Sent Video otherwise /d __\n\n__And make me admin in this channel then i can able to Upload otherwise i can't__")
+    input7: Message = await bot.listen(editable.chat.id)
+    raw_text7 = input7.text
+    if "/d" in input7.text:
+        channel_id = m.chat.id
+    else:
+        channel_id = input7.text
+    await input7.delete()     
+    await editable.delete()
+
+    if "/d" in raw_text7:
+        batch_message = await m.reply_text(f"<blockquote>🎯Target Batch : {b_name}<blockquote>")
+    else:
+        try:
+            batch_message = await bot.send_message(chat_id=channel_id, text=f"<blockquote>🎯Target Batch : {b_name}<blockquote>")
+            await bot.send_message(chat_id=m.chat.id, text=f"<blockquote>🎯Target Batch : {b_name}<blockquote>\n\n🔄 Your Task is under processing, please check your Set Channel📱. Once your task is complete, I will inform you 📩")
+        except Exception as e:
+            await m.reply_text(f"**Fail Reason »** {e}\n")
+            return
+        
+    failed_count = 0
+    count =int(raw_text)    
+    arg = int(raw_text)
+    try:
+        for i in range(arg-1, len(links)):
+            Vxy = links[i][1].replace("file/d/","uc?export=download&id=").replace("www.youtube-nocookie.com/embed", "youtu.be").replace("?modestbranding=1", "").replace("/view?usp=sharing","")
+            url = "https://" + Vxy
+            link0 = "https://" + Vxy
+
+            name1 = links[i][0].replace("(", "[").replace(")", "]").replace("_", "").replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@", "").replace("*", "").replace(".", "").replace("https", "").replace("http", "").strip()
+            name = f'{name1[:60]}'
+            
+            if "visionias" in url:
+                async with ClientSession() as session:
+                    async with session.get(url, headers={'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'Accept-Language': 'en-US,en;q=0.9', 'Cache-Control': 'no-cache', 'Connection': 'keep-alive', 'Pragma': 'no-cache', 'Referer': 'http://www.visionias.in/', 'Sec-Fetch-Dest': 'iframe', 'Sec-Fetch-Mode': 'navigate', 'Sec-Fetch-Site': 'cross-site', 'Upgrade-Insecure-Requests': '1', 'User-Agent': 'Mozilla/5.0 (Linux; Android 12; RMX2121) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36', 'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"', 'sec-ch-ua-mobile': '?1', 'sec-ch-ua-platform': '"Android"',}) as resp:
+                        text = await resp.text()
+                        url = re.search(r"(https://.*?playlist.m3u8.*?)\"", text).group(1)
+
+            if "acecwply" in url:
+                cmd = f'yt-dlp -o "{name}.%(ext)s" -f "bestvideo[height<={raw_text2}]+bestaudio" --hls-prefer-ffmpeg --no-keep-video --remux-video mkv --no-warning "{url}"'
+
+            elif "https://cpvod.testbook.com/" in url:
+                url = url.replace("https://cpvod.testbook.com/","https://media-cdn.classplusapp.com/drm/")
+                url = 'https://dragoapi.vercel.app/classplus?link=' + url
+                mpd, keys = helper.get_mps_and_keys(url)
+                url = mpd
+                keys_string = " ".join([f"--key {key}" for key in keys])
+
+            elif "classplusapp.com/drm/" in url:
+                url = 'https://dragoapi.vercel.app/classplus?link=' + url
+                mpd, keys = helper.get_mps_and_keys(url)
+                url = mpd
+                keys_string = " ".join([f"--key {key}" for key in keys])
+
+            elif "tencdn.classplusapp" in url:
+                headers = {'Host': 'api.classplusapp.com', 'x-access-token': f'{token_cp}', 'user-agent': 'Mobile-Android', 'app-version': '1.4.37.1', 'api-version': '18', 'device-id': '5d0d17ac8b3c9f51', 'device-details': '2848b866799971ca_2848b8667a33216c_SDK-30', 'accept-encoding': 'gzip'}
+                params = (('url', f'{url}'))
+                response = requests.get('https://api.classplusapp.com/cams/uploader/video/jw-signed-url', headers=headers, params=params)
+                url = response.json()['url']  
+
+            elif 'videos.classplusapp' in url or "tencdn.classplusapp" in url or "webvideos.classplusapp.com" in url:
+                url = requests.get(f'https://api.classplusapp.com/cams/uploader/video/jw-signed-url?url={url}', headers={'x-access-token': f'{token_cp}'}).json()['url']
+            
+            elif 'media-cdn.classplusapp.com' in url or 'media-cdn-alisg.classplusapp.com' in url or 'media-cdn-a.classplusapp.com' in url: 
+                headers = { 'x-access-token': f'{token_cp}',"X-CDN-Tag": "empty"}
+                response = requests.get(f'https://api.classplusapp.com/cams/uploader/video/jw-signed-url?url={url}', headers=headers)
+                url   = response.json()['url']
+
+            elif "childId" in url and "parentId" in url:
+                url = f"https://anonymousrajputplayer-9ab2f2730a02.herokuapp.com/pw?url={url}&token={raw_text4}"
+                           
+            elif "d1d34p8vz63oiq" in url or "sec1.pw.live" in url:
+                url = f"https://anonymouspwplayer-b99f57957198.herokuapp.com/pw?url={url}?token={raw_text4}"
+
+            if ".pdf*" in url:
+                url = f"https://dragoapi.vercel.app/pdf/{url}"
+            
+            elif 'encrypted.m' in url:
+                appxkey = url.split('*')[1]
+                url = url.split('*')[0]
+                
+                # Special handling for AppX .m3u8 links
+                # Special handling for AppX .m3u8 links
+                if "transcoded-videos.livelearn.in" in url or "appx-transcoded-videos" in url:
+                    cmd = f'yt-dlp --hls-prefer-native "{url}" -o "{name}.mp4"'
+                cmd = f'yt-dlp --hls-prefer-native "{url}" -o "{name}.mp4"'
+                
+            if "youtu" in url:
+                ytf = f"bv*[height<={raw_text2}][ext=mp4]+ba[ext=m4a]/b[height<=?{raw_text2}]"
+            elif "embed" in url:
+                ytf = f"bestvideo[height<={raw_text2}]+bestaudio/best[height<={raw_text2}]"
+            else:
+                ytf = f"b[height<={raw_text2}]/bv[height<={raw_text2}]+ba/b/bv+ba"
+           
+            if "jw-prod" in url:
+                cmd = f'yt-dlp -o "{name}.mp4" "{url}"'
+            elif "webvideos.classplusapp." in url:
+               cmd = f'yt-dlp --add-header "referer:https://web.classplusapp.com/" --add-header "x-cdn-tag:empty" -f "{ytf}" "{url}" -o "{name}.mp4"'
+            elif "youtube.com" in url or "youtu.be" in url:
+                cmd = f'yt-dlp --cookies youtube_cookies.txt -f "{ytf}" "{url}" -o "{name}".mp4'
+            else:
+                cmd = f'yt-dlp -f "{ytf}" "{url}" -o "{name}.mp4"'
+
+            try:
+                cc = f'╭━━━━━━━━━━━╮\n**🎥 VIDEO ID :** {str(count).zfill(3)}.\n╰━━━━━━━━━━━╯\n\n**📄 Title : {name1}** ({res}) ᴍʀꜱᴛʀᴀɴɢᴇʀ™.mkv\n\n**🔖 Batch :** `{b_name}`\n\n**📥 Extracted By :** {CR}'
+                #cpw = f'╭━━━━━━━━━━━╮\n**🎥 VIDEO ID :** {str(count).zfill(3)}.\n╰━━━━━━━━━━━╯\n\n**📄 Title : {name1}** ({res}) ᴍʀꜱᴛʀᴀɴɢᴇʀ™.mkv\n\n**🔗Video Url :** <a href="{url}">__Click Here to Watch Video__</a>\n\n\n**🔖 Batch :** `{b_name}`\n\n**📥 Extracted By :** {CR}'
+                #cyt = f'╭━━━━━━━━━━━╮\n**🎥 VIDEO ID :** {str(count).zfill(3)}.\n╰━━━━━━━━━━━╯\n\n**📄 Title : {name1}** ({res}) ᴍʀꜱᴛʀᴀɴɢᴇʀ™.mp4\n\n**🔗 Video Url :** <a href="{url}">__Click Here to Watch Video__</a>\n\n**🔖 Batch :** `{b_name}`\n\n**📥 Extracted By :** {CR}'
+                cpvod = f'╭━━━━━━━━━━━╮\n**🎥 VIDEO ID :** {str(count).zfill(3)}.\n╰━━━━━━━━━━━╯\n\n**📄 Title : {name1}** ({res}) ᴍʀꜱᴛʀᴀɴɢᴇʀ™.mkv\n\n**🔗Video Url :** <a href="{url}">__Click Here to Watch Video__</a>\n\n\n**🔖 Batch :** `{b_name}`\n\n**📥 Extracted By :** {CR}'
+                cimg = f'╭━━━━━━━━━━━╮\n**📁 IMAGE ID :** {str(count).zfill(3)}.\n╰━━━━━━━━━━━╯\n\n**📄 Title : {name1}** ᴍʀꜱᴛʀᴀɴɢᴇʀ™.jpg\n\n**🔖 Batch :** `{b_name}`\n\n**📥 Extracted By :** {CR}'
+                cczip = f'╭━━━━━━━━━━━╮\n**📁 FILE ID :** {str(count).zfill(3)}.\n╰━━━━━━━━━━━╯\n\n**📄 Title : {name1}** ᴍʀꜱᴛʀᴀɴɢᴇʀ™.zip\n\n**🔖 Batch :** `{b_name}`\n\n**📥 Extracted By :** {CR}'
+                cc1 = f'╭━━━━━━━━━━━╮\n**📁 FILE ID :** {str(count).zfill(3)}.\n╰━━━━━━━━━━━╯\n\n**📄 Title : {name1}** ᴍʀꜱᴛʀᴀɴɢᴇʀ™.pdf\n\n**🔖 Batch :** `{b_name}`\n\n**📥 Extracted By :** {CR}'
+                
+                if "drive" in url:
+                    try:
+                        ka = await helper.download(url, name)
+                        copy = await bot.send_document(chat_id=channel_id,document=ka, caption=cc1)
+                        count+=1
+                        os.remove(ka)
+                    except FloodWait as e:
+                        await m.reply_text(str(e))
+                        time.sleep(e.x)
+                        continue    
+  
+                elif ".pdf" in url:
+                    if "cwmediabkt99" in url:
+                        max_retries = 15  # Define the maximum number of retries
+                        retry_delay = 4  # Delay between retries in seconds
+                        success = False  # To track whether the download was successful
+                        failure_msgs = []  # To keep track of failure messages
+                        
+                        for attempt in range(max_retries):
+                            try:
+                                await asyncio.sleep(retry_delay)
+                                url = url.replace(" ", "%20")
+                                scraper = cloudscraper.create_scraper()
+                                response = scraper.get(url)
+
+                                if response.status_code == 200:
+                                    with open(f'{name}.pdf', 'wb') as file:
+                                        file.write(response.content)
+                                    await asyncio.sleep(retry_delay)  # Optional, to prevent spamming
+                                    copy = await bot.send_document(chat_id=channel_id, document=f'{name}.pdf', caption=cc1)
+                                    count += 1
+                                    os.remove(f'{name}.pdf')
+                                    success = True
+                                    break  # Exit the retry loop if successful
+                                else:
+                                    failure_msg = await m.reply_text(f"Attempt {attempt + 1}/{max_retries} failed: {response.status_code} {response.reason}")
+                                    failure_msgs.append(failure_msg)
+                                    
+                            except Exception as e:
+                                failure_msg = await m.reply_text(f"Attempt {attempt + 1}/{max_retries} failed: {str(e)}")
+                                failure_msgs.append(failure_msg)
+                                await asyncio.sleep(retry_delay)
+                                continue 
+                        for msg in failure_msgs:
+                            await msg.delete()
+                            
+else:
